3baadcd72f1dcdc396182d8066bbd5bc1512f6e8
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-07-08 20:01:42 +0530

    second commit- Added state and prop

--- todo/src/components/Container.js
+++ todo/src/components/Container.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Todos from './Todos.js'
 import Addtodo from './Addtodo.js'
+import './style.css'
 
 class Container extends React.Component{
+	state={
+		tasks:[
+			{ name: "I am task 1"},
+			{ name: "I am task 2"},
+			{ name: "I am task 3"},
+		]
+	}
+	
 	render(){
 		return(
 			<div className="container">
-				<Todos/>
+			    To Do App
+
+			    {this.state.tasks.map((task)=><Todos name= {task.name}/>)}
 				<Addtodo/>
 			</div>
 		);
--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './style.css'
 
 class Todos extends React.Component{
 	render(){
 		return(
 			<div className="todos">
-
+					{this.props.name}
 			</div>
 		);
 	}
