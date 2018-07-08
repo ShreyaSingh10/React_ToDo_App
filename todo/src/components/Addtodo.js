4f384e79b077fd9b57634bced471982a54d2b4d7
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-07-08 22:49:20 +0530

    Third Commit - used setState and event handlers

--- todo/src/components/Addtodo.js
+++ todo/src/components/Addtodo.js
@@ -2,11 +2,39 @@ import React from 'react';
 import './style.css'
 
 class Addtodo extends React.Component{
+	state={
+		name:''
+	}
+	handleChange = e =>{
+			console.log(e.target.value)
+
+			const  name  = e.target.value;
+			
+			this.setState(()=>{
+				return{
+					name
+				}
+			})
+
+			console.log(this.state);
+
+	}
+
+	handleSubmit = e =>{
+		e.preventDefault();
+		const todo ={
+			name:''
+		}
+		this.props.addToDo(todo);
+
+	}
 	render(){
 		return(
 			<div className="addtodo">
-				<input type="text" placeholder="Add your task"></input>
-				<button type="submit">Add Task</button>
+			<form onSubmit={this.handleSubmit}>
+				<input onChange ={this.handleChange} type="text" placeholder="Add your task"></input>
+				<button type="submit" type="submit">Add Task</button>
+			</form>
 			</div>
 		);
 	}
--- todo/src/components/Container.js
+++ todo/src/components/Container.js
@@ -11,14 +11,19 @@ class Container extends React.Component{
 			{ name: "I am task 3"},
 		]
 	}
+
+	addToDo = () => {
+		console.log("Adding TODO");
+
+	}
 	
 	render(){
 		return(
 			<div className="container">
 			    To Do App
 
-			    {this.state.tasks.map((task)=><Todos name= {task.name}/>)}
-				<Addtodo/>
+			    {this.state.tasks.map((task)=><Todos name={task.name}/>)}
+				<Addtodo addToDo={this.addToDo}/>
 			</div>
 		);
 	}
