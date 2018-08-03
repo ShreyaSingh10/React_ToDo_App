0b06a799cc200328795b15f0f41681d498a7cdc4
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-08-03 15:00:18 +0530

    Added prettier to format the code

--- src/components/Addtodo.js
+++ src/components/Addtodo.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import './style.css'
+import './style.css';
 
-class Addtodo extends React.Component{
-	state={
-		name:''
-	}
+class Addtodo extends React.Component {
+  state = {
+    name: ''
+  };
 
-	handleChange = e =>{
-			const  name  = e.target.value;
-			this.setState({
-					name
-				})
-	}
+  handleChange = e => {
+    const name = e.target.value;
+    this.setState({
+      name
+    });
+  };
 
-	handleSubmit = e =>{
-		e.preventDefault();
-		const todo ={
-			name: this.state.name,
-			visible:true,
-		}
-		this.props.addToDo(todo);
-	}
+  handleSubmit = e => {
+    e.preventDefault();
+    const todo = {
+      name: this.state.name,
+      visible: true
+    };
+    this.props.addToDo(todo);
+  };
 
-
-	render(){
-		return(
-			<div className="addtodo">
-			<form onSubmit={this.handleSubmit}>
-				<input id="add-input"onChange ={this.handleChange} type="text" placeholder="Add your task"></input>
-				<button type="submit"> Add </button>
-			</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="addtodo">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="add-input"
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Add your task"
+          />
+          <button type="submit"> Add </button>
+        </form>
+      </div>
+    );
+  }
 }
 export default Addtodo;
